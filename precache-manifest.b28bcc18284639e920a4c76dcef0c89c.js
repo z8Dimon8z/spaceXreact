3d9ea3571e92ba0aa356771502d8891293d0f626
Dima Lysak <60256445+z8Dimon8z@users.noreply.github.com>
2020-10-17 07:31:38 +0300

    залил файлы

---
+++ precache-manifest.b28bcc18284639e920a4c76dcef0c89c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0bd227c1e6b7dc2a0347b1c47a4d54e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f9e22dba8886b259b54",
+    "url": "/static/css/main.e3a8ffd6.chunk.css"
+  },
+  {
+    "revision": "bcb841f6556f18fe253c",
+    "url": "/static/js/2.8bcb97b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8bcb97b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f9e22dba8886b259b54",
+    "url": "/static/js/main.cc99b756.chunk.js"
+  },
+  {
+    "revision": "5268b1c7be79030bb5b5",
+    "url": "/static/js/runtime-main.142b1330.js"
+  }
+]);
